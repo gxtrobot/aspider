3626315af1cecee9603c88226938f9ad608a8407
gxtrobot <gxtrobot@gmail.com>
2019-10-12 16:25:35 +0800

    bump to 0.1.2

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,14 @@
 # Sample Test passing with nose and pytest
+import json
+
 
 def test_pass():
-        assert True, "dummy sample test"
+    assert True, "dummy sample test"
+
+
+def test_read_json_lines():
+    file = 'top250.json'
+    with open(file) as f:
+        for line in f:
+            d = json.loads(line)
+            print(d.keys())
